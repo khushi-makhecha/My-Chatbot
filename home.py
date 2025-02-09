b994122358a9a32077eb05c8764b90529f1c202f
riser515 <makhechakhushi@gmail.com>
2025-02-09 18:08:57 -0500

    Updated write() to subheader() on home page

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ st.title("Welcome to my first AI Chatbot")
 
 st.divider()
 
-st.write("Interact with different versions of the chatbot from the sidebar")
+st.subheader("Interact with different versions of the chatbot from the sidebar")
 
 st.image("https://media.licdn.com/dms/image/D4E12AQEyX2Em7OOEqg/article-cover_image-shrink_720_1280/0/1698523215001?e=2147483647&v=beta&t=MyGxuoB3cN63FG2cCxkNm3krzc19KUf6hAkwYSqKuM0")
 
